1a50327b86eebf14f50dca19f5a2da9794d62472
gillandghaz <gilland2015@gmail.com>
2026-08-21 09:09:13 +0700

    Latihan awal: hello world, variabel, input scanner

---
+++ 02-variabel/DataDiri.java
@@ -0,0 +1,13 @@
+public class DataDiri {
+    public static void main(String[] args) {
+        String nama = "Ayam";
+        int UMUR = 30;
+        Double pi = 3.14;
+        Boolean mahasiswabaru = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + UMUR);
+        System.out.println("Pi: " + pi);
+        System.out.println("Mahasiswa: " + mahasiswabaru);
+    }
+}
---
+++ 03-input-scanner/LatihanInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LatihanInput{
+    public static void main(String[] args){
+        Scanner p = new Scanner (System.in);
+
+        System.out.print("Siapa Namamu?");
+        String nama = p.nextLine();
+
+        System.out.print("Berapa Umurmu?");
+        int umur = x.nextInt();
+
+        System.out.println("Halo, " + nama + "!");
+        System.out.println("Umur: " + umur);
+    }
+}
